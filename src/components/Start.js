e09bbd64aa586f8adc71302fe2ac7f0dac71b150
ItsmeBlackOps <109592102+ItsmeBlackOps@users.noreply.github.com>
2023-10-25 14:28:25 -0400

    Update Start.js

--- src/components/Start.js
+++ src/components/Start.js
@@ -27,7 +27,7 @@ const Start = () => {
             <div className="container">
                 <div className="row vh-100 align-items-center justify-content-center">
                     <div className="col-lg-8">
-                        <h1 className='fw-bold mb-4'>Basic React JS Quiz</h1>
+                        <h1 className='fw-bold mb-4'>Pandas Quiz</h1>
                         <div className="mb-3">
                             <input
                                 type="text"
